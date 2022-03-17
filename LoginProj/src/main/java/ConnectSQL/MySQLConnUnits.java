2844391b232b44d34398ce99d1127e978bd8e009
Minh <binhminh150301@gmail.com>
2022-03-18 00:15:50 +0700

    prac

--- LoginProj/src/main/java/ConnectSQL/MySQLConnUnits.java
+++ LoginProj/src/main/java/ConnectSQL/MySQLConnUnits.java
@@ -1,5 +1,28 @@
 package ConnectSQL;
 
-public class MySQLConnUnits {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySQLConnUnits {
+	public static Connection getMySQLConnection()
+		throws ClassNotFoundException,SQLException {
+		String hostName = "localhost";
+		String 	dbName = "mysql";
+		String userName = "root";
+		String password = "rop12345678";
+		return getMySQLConnection(hostName, dbName, userName, password);
+	}
+	
+	public static Connection getMySQLConnection(String hostName, String dbName,
+	         String userName, String password) throws SQLException,
+	         ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+		
+		Connection conn = DriverManager.getConnection(connectionURL,userName,password);
+		return conn;
+		
+	}
 }
